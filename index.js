788aaa1574cdb95aa788bc2c3d39500676cea31e
maudnals <maud.nalpas@gmail.com>
2018-01-20 11:18:19 +0100

    trained

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // ------------------------------------------------
 // The beginning of everything
-// Create an object
+// Create an object basics: the object literal
 // ------------------------------------------------
 (function () {
 
   let maud = {
     name: "maud",
-    age: 26
+    age: 26,
+    getName: function () {
+      return this.name;
+    }
   };
 
 })();
+
 
 // ------------------------------------------------
 // Create objects that have the same structure
@@ -27,10 +31,11 @@
   }
   let maud = createPerson("maud", 26);
   let john = createPerson("john", 28);
-  console.log(maud);
-  console.log(john);
+  // maud.__proto__ and john.__proto = simple Object
 }());
 // But... getName method exists 2* in memory! => need something better.
+
+
 
 // ------------------------------------------------
 // One step further:
@@ -76,18 +81,20 @@
 
 // Option B2: like B1 but lighter syntax with Object.create() and a factory function
 (function () {
-  let person = {
-    name: "default",
-    age: null,
-    getName: function () {
-      return this.name;
-    }
-  }
   function createPerson(name, age) {
-    let newPerson = Object.create(person);
-    newPerson.name = name;
-    newPerson.age = age;
-    return newPerson;
+    // closure
+    let person = {
+      name: "default",
+      age: null,
+      getName: function () {
+        return this.name;
+      }
+    }
+    return Object.assign(Object.create(person), {
+      // or shorthand name, age
+      name: name,
+      age: age
+    });
   }
   let maud = createPerson("maud", 26);
   let john = createPerson("john", 28);
@@ -111,8 +118,44 @@
   let maud = new Person("maud", 26);
   let john = new Person("john", 28);
   // maud.__proto__ = john.__proto = Person's prototype
-  console.log(maud);
-  console.log(john);
+  // and automatically methods are in the __proto__
 }());
 
 
+
+// ------------------------------------------------
+// Compose objects
+// ------------------------------------------------
+
+// Option A
+
+class Identity {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  getName() {
+    return this.name;
+  }
+}
+class Improvisator {
+  constructor(name, age, improStyle) {
+    this.identity = new Identity(name, age);
+    this.improStyle = "savage";
+  }
+  // ... stay flexible
+  // => construct always small objects so that it's easy to compose
+}
+// DON'T: DONT INHERIT (`extend`) FROM PERSON, THAT WOULD BE CLASSICAL INHERITANCE,
+// AND IN JS WE FAVOR COMPOSITION AND PROTOTYPAL INHERITANCE OVER CLASSICAL INHERITANCE.
+// One case where that's OK is when creating a Y that is really a X. That's OK: class MyApp extends React.Component (MyApp really is a react component, nothing more, nothing less). But because a JS class is already an object, keep in mind that Y inherits from X which exists.
+
+ // Option B: mixins
+
+
+
+// ------------------------------------------------
+// Other object operations
+// ------------------------------------------------
+
+// Copy objects with object.assign
