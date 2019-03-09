fb56bef8192bbb001e47942049d718eefa1733a7
maudnals <maud.nalpas@gmail.com>
2019-03-09 11:24:50 +0100

    feat(closures): improved examples

--- closures/closures.js
+++ closures/closures.js
@@ -1,83 +1,72 @@
 // WHAT IS
 // A closure is a stateful function
 
-(function () {
+(function() {
   function greet(name) {
-    return function () {
+    return function() {
       console.log('hi, ' + name);
-    }
+    };
   }
 
-  greet("maud")();
+  greet('maud')();
 
-  const greetMaud = greet("maud");
+  const greetMaud = greet('maud');
   greetMaud();
 })();
 
-console.log("--------------");
+console.log('--------------');
 
 /**
  * USE CASE #1: function factories
- * = function factories can be used to create functions with the same execution but a different environment
+ * = function factories can be used to create functions with the same execution context but a different environment
  */
-(function () {
-
-  function greet(greeting, name) {
-    return function (name) {
-      console.log(`${greeting}, ${name}`);
-    }
+(function() {
+  function greetFactory(greeting) {
+    return name => `${greeting}, ${name}`;
   }
-
-  const greetEnglish = greet("hi");
-  const greetFrench = greet("salut");
-  greetFrench("maud");
+  const greetEnglish = greet('hi');
+  const greetFrench = greet('salut');
 })();
 
-console.log("--------------");
+console.log('--------------');
 
 /**
  * USE CASE #2: revealing module pattern (OBJECT PRIVACY)
  */
 
-(function () {
-  const maud = (function () {
+const building = name =>
+  (function() {
     const secretCode = 42;
     function getPublicCode() {
       return secretCode * 2;
     }
     return {
-      name: "maud",
-      age: 26,
-      gender: "F",
-      official: function () {
-        return (getPublicCode());
-      }
-    }
-  }
-  )();
-  console.log(maud);
-  console.log(maud.official());
-})();
-
+      name,
+      getPublicCode
+    };
+  })();
+// We can access the public code, but not the private one!
+console.log(building('Allianz Arena').getPublicCode());
 
 /**
  * USE CASE #3: see/understand the call stack
  * (like redux isDispatching)
  */
+// ... not really...
 
-(function () {
+(function() {
   function doThis() {
     let isWalking = false;
     function walk() {
       isWalking = true;
-      playTEDTalk();
+      playVideo();
     }
-    function playTEDTalk() {
+    function playVideo() {
       // thanks to the closure we now have a view on the call stack!
-      // we know where the playTEDTalk call came from:
-      //    playTEDTalk
+      // we know where the playVideo call came from:
+      //    playVideo
       //    walk
-      // So we can make playTEDTalk behave accordingly, for example:
+      // So we can make playVideo behave accordingly, for example:
       if (isWalking) {
         // audio only
       } else {
